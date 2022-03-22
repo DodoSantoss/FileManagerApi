73b5e53bdc0dc037ae7f35b8b845c2c8357c8cd0
douglas.santos <douglas.santos@NOTE7-MC1400>
2022-03-22 09:41:00 -0300

    Add project files.

---
+++ DriveServices/AuthService.cs
@@ -0,0 +1,57 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace DriveServices
+{
+    public class AuthService
+    {
+        [Obsolete]
+        public static DriveService AuthenticateOauth(string clientSecretJson, string username)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(username))
+                    throw new ArgumentNullException("userName");
+                if (string.IsNullOrEmpty(clientSecretJson))
+                    throw new ArgumentNullException("clientSecretJson");
+                if (!File.Exists(clientSecretJson))
+                    throw new Exception("clientSecretJson file does not exist.");
+
+                // These are the scopes of permissions you need. It is best to request only what you need and not all of them
+                string[] scopes = new string[] { DriveService.Scope.DriveReadonly };//View the files in your Google Drive                                                 
+                UserCredential credential;
+                using (var stream = new FileStream(clientSecretJson, FileMode.Open, FileAccess.Read))
+                {
+                    string credentialPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                    credentialPath = Path.Combine(credentialPath, ".credentials/", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+
+                    // Requesting Authentication or loading previously stored authentication for userName
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets,
+                                                                             scopes,
+                                                                             username,
+                                                                             CancellationToken.None,
+                                                                             new FileDataStore(credentialPath, true)).Result;
+                }
+
+                // Create Drive API service.
+                return new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Drive Oauth2 Authentication Sample"
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Create Oauth2 account DriveService failed" + ex.Message);
+                throw new Exception("CreateServiceAccountDriveFailed", ex);
+            }
+        }
+
+    }
+}
+
---
+++ DriveServices/FileService.cs
@@ -0,0 +1,70 @@
+﻿using Google.Apis.Drive.v3;
+using System;
+using System.IO;
+
+namespace DriveServices
+{
+    public class FileService
+    {
+        private readonly DriveService _service;
+        private readonly Google.Apis.Drive.v3.Data.File _file;
+        public FileService(DriveService service, Google.Apis.Drive.v3.Data.File file)
+        {
+            _service = service;
+            _file = file;
+        }
+        private void DownloadFile(string saveTo)
+        {
+            var request = _service.Files.Get(_file.Id);
+            var stream = new MemoryStream();
+
+            // Add a handler which will be notified on progress changes.
+            // It will notify on each chunk download and when the
+            // download is completed or failed.
+            request.MediaDownloader.ProgressChanged += (Google.Apis.Download.IDownloadProgress progress) =>
+            {
+                switch (progress.Status)
+                {
+                    case Google.Apis.Download.DownloadStatus.Downloading:
+                        {
+                            Console.WriteLine(progress.BytesDownloaded);
+                            break;
+                        }
+                    case Google.Apis.Download.DownloadStatus.Completed:
+                        {
+                            Console.WriteLine("Download complete.");
+                            SaveStream(stream, saveTo);
+                            break;
+                        }
+                    case Google.Apis.Download.DownloadStatus.Failed:
+                        {
+                            Console.WriteLine("Download failed.");
+                            break;
+                        }
+                }
+            };
+
+            request.Download(stream);
+        }
+
+        private static void SaveStream(MemoryStream stream, string saveTo)
+        {
+            using FileStream file = new FileStream(saveTo, FileMode.Create, FileAccess.Write);
+            stream.WriteTo(file);
+        }
+
+        private void ConnectToDrive()
+        {
+            // Connect to Google
+            var service = GoogleSamplecSharpSample.Drivev3.Auth.Oauth2Example.AuthenticateOauth(@"[Location of the Json cred file]", "test");
+            //List the files with the word 'make' in the name.
+            var files = DriveListExample.ListFiles(service,
+                             new DriveListExample.FilesListOptionalParms() { Q = "name contains 'Make'", Fields = "*" });
+            foreach (var item in files.Files)
+            {
+                // download each file
+                DriveDownload.Download(service, item, string.Format(@"C:\FilesFromGoogleDrive\{0}", item.Name));
+            }
+        }
+    }
+}
